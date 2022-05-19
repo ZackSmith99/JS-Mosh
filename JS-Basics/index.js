8b6f254093534b30d6e8e14aa8e088911ba08821
dialecticsniff <Dialecticsniff@hotmail.com>
2022-05-19 16:29:08 -0700

    02 - Basic - 03 - Primitive Types

--- JS-Basics/index.js
+++ JS-Basics/index.js
@@ -1,8 +1,11 @@
-const interestRate = 0.3;
-interestRate = 1;
-console.log(interestRate);
-
-
-/*   let interestRate = 0.3;
-interestRate = 1;
-console.log(interestRate);  This displayed in the Consule as 1  */  
+/* 02- Basics - 03 - Primitive Types     */  
+let name = 'Mosh';  //This is a String Literal
+let age = 30;  //Number Literal
+let isApproved = true;  // Boolean Literal
+let firstName;
+let firstNames = undefined;  //undefined
+let lastNames = null; //null
+let selectedColor = null;  /*after a user selects a color, we ressign this variable to a color like red:
+// let selectedColor = 'red';
+Then, if the user clicks red again, we want remove the selection and set this back to null.
+*/  
