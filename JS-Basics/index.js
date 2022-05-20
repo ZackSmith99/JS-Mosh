8e27c616353820e9224c1a5ace2bd1c349f1a1bf
dialecticsniff <Dialecticsniff@hotmail.com>
2022-05-19 21:30:57 -0700

    02 - Basics - 04 - Dynamic Typing

--- JS-Basics/index.js
+++ JS-Basics/index.js
@@ -1,11 +1,6 @@
-/* 02- Basics - 03 - Primitive Types     */  
+/* 02 - Basics - 04 - Dynamic Typing     */  
 let name = 'Mosh';  //This is a String Literal
 let age = 30;  //Number Literal
 let isApproved = true;  // Boolean Literal
-let firstName;
-let firstNames = undefined;  //undefined
-let lastNames = null; //null
-let selectedColor = null;  /*after a user selects a color, we ressign this variable to a color like red:
-// let selectedColor = 'red';
-Then, if the user clicks red again, we want remove the selection and set this back to null.
-*/  
+let firstName = undefined;  //undefined
+let selectedColor = null;
