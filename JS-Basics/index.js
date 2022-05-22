27814eb50a66b6920add9d6c417b68d9cc72e39c
dialecticsniff <Dialecticsniff@hotmail.com>
2022-05-21 18:21:01 -0700

    03 - Operators - 04 - Comparison Operators

--- JS-Basics/index.js
+++ JS-Basics/index.js
@@ -1,46 +1,11 @@
-/*
- 
-This function is Performing a task.
-function greet(name, lastName) {
-    console.log('Hello ' + name + ' ' + lastName);
-}
+let x = 1;
 
-greet('John', 'Smith');
+//Relational
+console.log(x > 0);
+console.log(x >= 1);
+console.log(x < 1);
+console.log(x <= 1);
 
-
-
-function square(number) {
-    return number * number;
-}
-
-//We were calling the greet function.  Now we are calling the square function.
-let number = square(2);
-console.log(number);
-
-
-Soo... we'll call the function Square.  Square has a number parameter.  We will return number times number.
-
-Our variable says that number will equal square function (N times N), with the number parameter being 2.  There is an order of operations aspect.  The same term can have different values on the page... because sometimes that term is being acted upon, and other... things are receiving that finished output  
-
-function square(number) {
-    return number * number;
-}
-
-let number = square(2);
-console.log(number);
-
-
-*/
-//Performing a task
-function greet(name, lastName) {
-    console.log('Hello ' + name + ' ' + lastName);
-}
-
-
-function square(number) {
-    return number * number;
-}
-
-
-console.log(square(2));
-
+//Equality
+console.log(x === 1);
+console.log(x !== 1);
