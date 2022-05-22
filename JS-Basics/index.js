60c9bef3e74fa1672903cbe61cbf2a29be805c7c
dialecticsniff <Dialecticsniff@hotmail.com>
2022-05-21 19:12:10 -0700

    03 - Operators - 05 - Equality Operators

--- JS-Basics/index.js
+++ JS-Basics/index.js
@@ -1,11 +1,15 @@
-let x = 1;
+/* 
+// Strict Equality Operator (Type + Value)
+console.log(1 === 1);
+console.log('1' === 1);
 
-//Relational
-console.log(x > 0);
-console.log(x >= 1);
-console.log(x < 1);
-console.log(x <= 1);
 
-//Equality
-console.log(x === 1);
-console.log(x !== 1);
+// Lose Eqaulity Operator
+console.log(1 == 1);
+
+*/
+
+// Lose Eqaulity Operator
+console.log(1 == 1);
+console.log('1' == 1);
+console.log(true == 1);
