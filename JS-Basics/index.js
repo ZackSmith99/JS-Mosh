0fde33d2266af62061f7bba6cb870ce4d7647fa1
dialecticsniff <Dialecticsniff@hotmail.com>
2022-05-28 21:53:12 -0700

    03 - Operators - 09 - Bitwise Operators

--- JS-Basics/index.js
+++ JS-Basics/index.js
@@ -1,42 +1,76 @@
 /*
 
-Falsy (false) :
-Undefined
-null
-0
-boolean false
-empty string ''
-Not a Number NaN
+Example of a bitwise or with a single vertical line: console.log(1 | 2);
+(Logical ors have double vertical lines ||)
 
-Anything that is not Falsy -> Truthy
-
-E.g.:
-
- false || ‘Mosh’
-returns: 'Mosh'
-
-false || 1
-We get an output of 1.
-
-false || 1 || 2
-The console outputs 1.  
-
-With the logical/or operator, it will return a result as soon as it finds an operand that is truthy.  
-
-You could have a dozen more operands… it will only return the first truthy operand it encounters.  This is called Short-Circuiting.
+// Bitwise Or  If Either of the bits are 1, the result will be 1.
+console.log(1 | 2);   
+1 = 0000001
+2 = 0000010
+R = 0000011
 
 
+// Bitwise And - If both numbers are one, one will be returned.  Otherwise, the result will be zero:
 
-let userColor = 'red';
-let defaultColor = 'blue';
-let currentColor = userColor || defaultColor;
+console.log(1 & 2); 
+1 = 0000001
+2 = 0000010
+3 = 0000011
+R = 0000000
 
-console.log(currentColor);
-The output is Red, because user color is selected
+
+console.log(1 | 2); 
+console.log(1 & 2); 
+
+The console outputs 3 and 0.
+
+These are the fundamentals.  
+
+
+//Read, Write, Exectute  (User features)
+//00000 (the first five bits are irrelevant).  The remaining bits indicate that the user has permission if the value is one.
+
+00000100 Read
+00000010 Write
+00000001
+
+
+const readPermission = 4;
+const writePermission = 2;
+const executePermission = 1;
+
+let myPermission =0;
+myPermission = myPermission | readPermission | writePermission;
+
+console.log(myPermission);
+((console displays 6))
+
+
+
+
+const readPermission = 4;
+const writePermission = 2;
+const executePermission = 1;
+
+let myPermission = 0;
+myPermission = myPermission | readPermission | writePermission;
+
+let message =
+ (myPermission & readPermission) ? 'yes': 'no';
+
+ console.log(message);
+Outputs to yes
+
 */
 
-let userColor = undefined;
-let defaultColor = 'blue';
-let currentColor = userColor || defaultColor;
+const readPermission = 4;
+const writePermission = 2;
+const executePermission = 1;
 
-console.log(currentColor);
+let myPermission = 0;
+myPermission = myPermission | writePermission;
+
+let message =
+ (myPermission & readPermission) ? 'yes': 'no';
+
+ console.log(message);
