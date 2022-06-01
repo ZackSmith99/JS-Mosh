50db53a47933eb61eb53f226a3c2f0fd7d52e0e1
dialecticsniff <Dialecticsniff@hotmail.com>
2022-05-31 17:33:18 -0700

    03 - Operators - 02 - Arithmetic Operators

--- JS-Basics/index.js
+++ JS-Basics/index.js
@@ -1,20 +1,57 @@
 /*
-let x = 2 + 3 * 4;
+
+let x = 10;
+let y = 3;
 
 console.log(x);
+Outputs 10
 
-The output is 14
 
 
-let x = (2 + 3) * 4;
+let x = 10;
+let y = 3;
 
-console.log(x);
+console.log(++x);
+Outputs 11
 
-This output is 20.
 
+let x = 10;
+let y = 3;
+
+console.log(x++);
+Outputs 10 for the First console, but X Has been incremented by one.
+
+
+
+
+let x = 10;
+let y = 3;
+
+console.log(x++);
+console.log(x)
+Displays 10, and 11 on console, because x is incremented by one after first console.log
+
+
+Decrement works in the same manner.
 */
+let x = 10;
+let y = 3;
+
+console.log(x++);
+console.log(x)
 
 
-let x = (2 + 3) * 4;
+// console.log(x + Y);
+// console.log(x - Y);
+// console.log(x * Y);
+// console.log(x / Y);
+// console.log(x % Y);  Remainer of division
+// console.log(x ** Y);  Exponential?  X to the power of Y
 
-console.log(x);
+// Increment (++)
+// Decrement (--)
+
+
+
+
+
