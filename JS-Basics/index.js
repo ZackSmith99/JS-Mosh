014d83142d98e6e8fb98353680beafd38f3750f0
dialecticsniff <Dialecticsniff@hotmail.com>
2022-06-03 18:57:52 -0700

    04 - Control Flow - 05 - Do...while

--- JS-Basics/index.js
+++ JS-Basics/index.js
@@ -1,38 +1,20 @@
 /*
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-Don't be dumb.
 
-
-this is how you do it.  
-for (let i = 0; i < 5; i++) {
-    console.log ('Hello World');
-}
-
-
-note the value of the statement displayed on conosole
-for (let i = 0; i < 5; i++) {
-    console.log ('Hello World', i);
-}
-
-
-We can start i with a value of 1
-for (let i = 1; i <= 5; i++) {
-    console.log ('Hello World', i);
-}
 */
 
-for (let i = 0; i <= 5; i++) {
+// for (let i = 0; i <= 5; i++) {
+//     if (i % 2 !== 0) console.log(i);
+// }
+
+
+// let i = 9;
+// while (i <= 5) {
+// if (i % 2 !== 0) console.log(i);
+// i++;
+// }
+
+let i = 9;
+do {
     if (i % 2 !== 0) console.log(i);
-}
-
-
-let i = 0;
-while (i <= 5) {
-if (i % 2 !== 0) console.log(i);
-i++;
-}
-
+     i++;
+} while (i <= 5);
