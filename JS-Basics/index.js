672bf175448a2b271e623ca187924f7913511c05
dialecticsniff <Dialecticsniff@hotmail.com>
2022-08-01 15:36:28 -0700

    04 - Control Flow - 08 - For..of

--- JS-Basics/index.js
+++ JS-Basics/index.js
@@ -5,46 +5,6 @@ age: 30
 };
 
 for (let key in person)
-console.log(key);
-//this displays "name" and then "age" in the second iteration
-
-
-
-const person = {
-    name: 'Mosh',
-age: 30
-};
-
-for (let key in person)
-console.log(key, person.key);
-//dot notation ((e.g., person.key)) does not work.  There is no property called key in //the person object.
-
-
-
-
-
-
-const person = {
-    name: 'Mosh',
-age: 30
-};
-
-for (let key in person)
-console.log(key, person[key]);
-
-//this 'passes' key as the name of the target property.  I think this means key stands in for or pretends to be the property of the person object, and it runs/iterates through them.  Now both the property, And the property's value is displayed.
-
-
-
-*/
-
-
-const person = {
-    name: 'Mosh',
-age: 30
-};
-
-for (let key in person)
 console.log(key, person[key]);
 
 const colors = ['red', 'green', 'blue']; 
@@ -52,4 +12,28 @@ const colors = ['red', 'green', 'blue'];
 for (let index in colors)
 console.log(index, colors[index]);
 
-//with console.log(index), the numerical values, i.e., the index varialbe is set to the index of one of the elements in the array... will be displayed.  Whereas, when colors[index] is included, the Element at a given index will be displayed  
+for (let color of colors)
+console.log(color);
+
+//with the for of loop, the color variable will 'hold' an item in the array as it iterates through.  It won't also display the index number, like with you use a for-in loop.
+
+
+*/
+
+//for-in
+const person = {
+    name: 'Mosh',
+age: 30
+};
+
+for (let key in person)
+console.log(key, person[key]);
+
+//for-of
+const colors = ['red', 'green', 'blue']; 
+
+for (let color of colors)
+console.log(color);
+
+//We use the for-in loop to iterate over the properties of an object. 
+//And the for-of loop to iterate over the elements, or items in an array.
