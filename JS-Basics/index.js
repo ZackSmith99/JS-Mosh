dfcc7969a58fc07781d2f5ec00c5053cef99f6b9
dialecticsniff <Dialecticsniff@hotmail.com>
2022-08-01 13:28:46 -0700

    04 - Control Flow - 07 For...in Loops

--- JS-Basics/index.js
+++ JS-Basics/index.js
@@ -1,4 +1,40 @@
 /*
+const person = {
+    name: 'Mosh',
+age: 30
+};
+
+for (let key in person)
+console.log(key);
+//this displays "name" and then "age" in the second iteration
+
+
+
+const person = {
+    name: 'Mosh',
+age: 30
+};
+
+for (let key in person)
+console.log(key, person.key);
+//dot notation ((e.g., person.key)) does not work.  There is no property called key in //the person object.
+
+
+
+
+
+
+const person = {
+    name: 'Mosh',
+age: 30
+};
+
+for (let key in person)
+console.log(key, person[key]);
+
+//this 'passes' key as the name of the target property.  I think this means key stands in for or pretends to be the property of the person object, and it runs/iterates through them.  Now both the property, And the property's value is displayed.
+
+
 
 */
 
@@ -9,5 +45,11 @@ age: 30
 };
 
 for (let key in person)
-console.log(person)
+console.log(key, person[key]);
 
+const colors = ['red', 'green', 'blue']; 
+
+for (let index in colors)
+console.log(index, colors[index]);
+
+//with console.log(index), the numerical values, i.e., the index varialbe is set to the index of one of the elements in the array... will be displayed.  Whereas, when colors[index] is included, the Element at a given index will be displayed  
