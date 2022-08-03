f1b7aa3f6062f960acd8d6c22f8eb7df8592fe52
dialecticsniff <Dialecticsniff@hotmail.com>
2022-08-03 13:38:45 -0700

    04 - Control Flow - Exercise 10 - Max of Two Numbers

--- JS-Basics/index.js
+++ JS-Basics/index.js
@@ -1,16 +1,4 @@
 /*
-let i = 0;
-while (i <= 10) {
-   if (i === 5) break
-    console.log(i);
-    i++;
-}
-//this discontinues counting at 4.
-
-
-
-
-*/
 
 let i = 0;
 while (i <= 10) {
@@ -23,4 +11,129 @@ while (i <= 10) {
     i++;
 }
 
+//10 and 20
+
+if (10 )
+let i = 10
+
+
+
+function 
+
+
+function greet (name, lastName) {
+    console.log('Hello  ' + name + ' ' + lastName);
+    
+}
+
+greet ('John', 'Smith');
+greet ('Mary')
+
+
+function square(number) {
+return number * number;
+}
+
+let number = square(2);
+console.log(number);
+
+function square(number) {
+return number * number;
+}
+
+
+console.log('bob');
+
+
+
+
+
+
+
+
+
+
+function maximum(number1, number2) {
+    if (number1 > number2) {console.log(number1)}
+    else
+console.log(number2)}
+}
+let number1 = 10
+let number2 = 20
+
+
+if (10>20) {console.log(10)}
+else 
+console.log(20)
+
+
+
+
+
+let number1 = 10
+let number2 = 20
+
+if (number1 > number2) {console.log(number1)}
+else 
+console.log(number2)
+
+
+
+
+function maximum(number1, number2) {
+    return number1 > number2;
+    return number1 < number2;
+    
+}
+
+console.log(maximum(10, 20));
+
+
+if (10>20) {console.log(10)}
+else 
+console.log(20)
+
+
+
+
+
+function maximum(number1, number2) {
+    if (number1 > number2) {return number1;
+    }
+    else return number2;
+}
+
+console.log(maximum(50, 20));
+
+
+let number = max(1, 2);
+console.log(number);
+
+function max(a, b) {
+if (a > b) return a;
+else return b;
+}
+
+
+
+let number = max(1, 2);
+console.log(number);
+
+function max(a, b) {
+if (a > b) return a;
+return b;
+}
+
+*/
+
+
+
+
+
+let number = max(1, 2);
+console.log(number);
+
+function max(a, b) {
+return (a > b) ? a : b;
+}
 
