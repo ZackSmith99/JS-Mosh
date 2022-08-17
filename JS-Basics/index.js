1479c7d2b52002a6ab0a739935d3478e378077d2
dialecticsniff <Dialecticsniff@hotmail.com>
2022-08-16 19:46:58 -0700

    04 - Control Flow - 14 - Exercise 5 - Even and Odd Numbers

--- JS-Basics/index.js
+++ JS-Basics/index.js
@@ -1,33 +1,46 @@
 /*
 
-My solution:
 
-checkSpeed(80);
+My solution
 
-function checkSpeed(speed) { if (speed <= 74) console.log('OK');
+showNumbers(100);
 
-else if (speed >= 75  && speed < 130)
-console.log('Point: '+ (Math.floor((speed -75)/5 + 1)) )
-
-else return console.log('License Suspended')
+function showNumbers(limit) {
+    if (typeof limit !== 'number')
+    return 
+     
+    for (let i = 0;
+    i <= limit; 
+    i++)
+    {if (i % 2 === 0) console.log(i + ' "EVEN"')
+    else if (i % 2 !== 0) console.log(i + ' "ODD"')
+}
 }
 
 
 
-*/
-checkSpeed(70);
 
-function checkSpeed(speed) {
-    const speedLimit = 70;
-    const kmPerPoint = 5;
-   
-    if (speed <= speedLimit + kmPerPoint) {
-        console.log('OK');
-        return;
-    }
-    const points = Math.floor((speed - speedLimit) / kmPerPoint);
-    if (points >= 12)
-    console.log('License suspended')
-    else
-    console.log('Points', points);
+
+first mosh solution
+showNumbers(10);
+
+function showNumbers(limit) {
+for (let i = 0; i <= limit; i++) {
+    if (i % 2 === 0) console.log(i, 'EVEN');
+    else console.log(i, 'ODD')
+
+}
+
+}
+*/
+
+
+showNumbers(10);
+
+function showNumbers(limit) {
+for (let i = 0; i <= limit; i++) {
+    
+const message = (i % 2 === 0) ? 'EVEN' : 'ODD';
+console.log(i, message);
+}
 }
