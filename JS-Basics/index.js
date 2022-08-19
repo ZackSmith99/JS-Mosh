8dc308d9fa947fd6a4d2af9ec3ff9e607080a5e8
dialecticsniff <Dialecticsniff@hotmail.com>
2022-08-18 20:44:58 -0700

    04 - Control Flow - 15 - Exercise 6 - Count Truthy

--- JS-Basics/index.js
+++ JS-Basics/index.js
@@ -1,46 +1,102 @@
 /*
 
 
-My solution
 
-showNumbers(100);
 
-function showNumbers(limit) {
-    if (typeof limit !== 'number')
-    return 
-     
-    for (let i = 0;
-    i <= limit; 
-    i++)
-    {if (i % 2 === 0) console.log(i + ' "EVEN"')
-    else if (i % 2 !== 0) console.log(i + ' "ODD"')
-}
+const array = [0, null, undefined, '', 1, 2, 3];
+
+console.log(countTruthy(array));
+
+function countTruthy(array) {
+return (array.length,)  
 }
 
 
 
 
 
-first mosh solution
-showNumbers(10);
+const array = [0, null, undefined, '', 1, 2, 3, 4];
 
-function showNumbers(limit) {
-for (let i = 0; i <= limit; i++) {
-    if (i % 2 === 0) console.log(i, 'EVEN');
-    else console.log(i, 'ODD')
+console.log(countTruthy(array));
+
+function countTruthy(array) {
+
+    let count = 0;
+
+    
 
 }
 
-}
+
+
+
+
+
+const person = {
+    name: 'Mosh',
+    age: 30
+
+};
+
+for (let bob in person)
+console.log(person[bob]);
+
+
+const colors = ['red', 'green', 'blue', 3, 5];
+
+for (let dave in colors)
+console.log(colors[dave]);
+
+for (let color of colors)
+if (typeof color == 'string')
+console.log(color)
+
+for (let color if colors)
+console.log(color);
+
+
+
+
+
+
+I have come up with this, so far.  
+const colors = ['red', 'green', 'blue', 3, 5];
+
+
+for (let color of colors)
+if (typeof color == 'number')
+console.log(color.length)
+
+
 */
 
 
-showNumbers(10);
 
-function showNumbers(limit) {
-for (let i = 0; i <= limit; i++) {
+
+
+
+
+
+
+
+
+
+
+const array = [0, null, undefined, '', 1, 2, 3, 4];
+
+console.log(countTruthy(array));
+
+function countTruthy(array) {
     
-const message = (i % 2 === 0) ? 'EVEN' : 'ODD';
-console.log(i, message);
-}
+    let bob = 0;
+    for (let value of array)
+    if (value === 'number' || value !== false || value !== 0 || value !== null || value !== undefined || value !== '') 
+    
+     
+    
+    bob++;
+    return bob;
+
+
+
 }
