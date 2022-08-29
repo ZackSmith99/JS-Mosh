356e5fcf720f79cd2eb2204b30bb9a78c8925665
dialecticsniff <Dialecticsniff@hotmail.com>
2022-08-29 13:18:12 -0700

    04 - Control Flow - 19 - Exercise 10 - Stars

--- JS-Basics/index.js
+++ JS-Basics/index.js
@@ -1,145 +1,100 @@
 /*
+const marks = [80, 80, 50];
 
+console.log(calculateGrade(marks));
 
+function calculateGrade(marks) {
 
+    let sum = 0;
+   
 
-const array = [0, null, undefined, '', 1, 2, 3];
-
-console.log(countTruthy(array));
-
-function countTruthy(array) {
-return (array.length,)  
-}
-
-
-
-
-
-const array = [0, null, undefined, '', 1, 2, 3, 4];
-
-console.log(countTruthy(array));
-
-function countTruthy(array) {
-
-    let count = 0;
-
+    for (let average of marks)
+    sum += average
     
+    if ( [sum / marks.length]  < 59)
+    {return 'F'}
+
+    else if (   [sum / marks.length] >= 60  && [sum / marks.length] <= 69  )
+    {return 'D'}
+
+    else if ( (sum / marks.length) >= 70 && (sum / marks.length) <= 79 )
+    {return 'C'}
+
+    else if (  (sum / marks.length) > 80 && (sum / marks.length) <= 89 )
+    {return 'B'}
+ 
+    {return 'A'}
 
 }
 
+const marks = [80, 80, 50];
 
+console.log(calculateGrade(marks));
 
+function calculateGrade(marks) {
 
+    let sum = 0;
+   
 
-
-const person = {
-    name: 'Mosh',
-    age: 30
-
-};
-
-for (let bob in person)
-console.log(person[bob]);
-
-
-const colors = ['red', 'green', 'blue', 3, 5];
-
-for (let dave in colors)
-console.log(colors[dave]);
-
-for (let color of colors)
-if (typeof color == 'string')
-console.log(color)
-
-for (let color if colors)
-console.log(color);
-
-
-
-
-
-
-I have come up with this, so far.  
-const colors = ['red', 'green', 'blue', 3, 5];
-
-
-for (let color of colors)
-if (typeof color == 'number')
-console.log(color.length)
-
-
-
-
-
-const array = [0, null, undefined, '', 1, 2, 3, 4];
-
-console.log(countTruthy(array));
-
-function countTruthy(array) {
+    for (let average of marks)
+    sum += average
     
-    let bob = 0;
-    for (let value of array)
-    if (value) 
-    
-     
-    
-    bob++;
-    return bob;
+    let points = (sum / marks.length)
+
+    if ( points  < 59)
+    {return 'F'}
+
+    else if (points >= 60  && points <= 69  )
+    {return 'D'}
+
+    else if (points >= 70 && points <= 79 )
+    {return 'C'}
+
+    else if (points > 80 && points <= 89 )
+    {return 'B'}
+
+    {return 'A'}
+
+
+
+    const marks = [80, 80, 50];
+
+console.log(calculateGrade(marks));
+
+function calculateGrade(marks) {
+    let sum = 0;
+    for (let mark of marks)
+        sum += mark;
+    let average = sum / marks.length;
+
+    if (average < 60) return 'F';
+    if (average < 70) return 'D';
+    if (average < 80) return 'C';
+    if (average < 90) return 'B';
+    return 'A';
+}
 }
 
 
-
-const movie = {
-    title: 'a',
-    releaseYear: 2018,
-    rating: 4.5,
-    director: 'b'
-};
-
-
-showProperties(movie);
-
-function showProperties(obj) { 
-    for (let key in obj)
-    if (typeof obj[key] === 'string')
-    console.log(key, obj[key]);
-}
-
-
-console.log(sum(10));
-
-function sum(limit) {
-
-for (let i = 0; i * 3 <= limit; i++);
-
-}
-console.log(sum(10));
-
-
-let fiver = 
-    let threes = 
 */
 
-console.log(sum(10));
+const marks = [80, 80, 50];
 
-function sum(limit) { 
-    
-    let i = 1
-    let sum = (fiver + threes);
-    let bob = (i * 3)
+console.log(calculateGrade(marks));
 
-
-   return bob
-
+function calculateGrade(marks) {
+    const average = calculateAverage(marks);
+    if (average < 60) return 'F';
+    if (average < 70) return 'D';
+    if (average < 80) return 'C';
+    if (average < 90) return 'B';
+    return 'A'; 
 }
 
 
-
-
-
-
-
-
-
-
-
+ function calculateAverage(array)  {
+        let sum = 0;
+        for (let value of array)
+            sum += value;
+        return sum / array.length;
+}
