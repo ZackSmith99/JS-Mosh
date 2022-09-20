6c5734277c8fdaf4cdb70eb34ce71ee0a5a2780f
dialecticsniff <Dialecticsniff@hotmail.com>
2022-09-19 17:39:44 -0700

    05 - Objects - 06 - Functions are Objects - 4.47

--- JS-Basics/index.js
+++ JS-Basics/index.js
@@ -1,29 +1,16 @@
 /*
 
-
-
-
-new String();  // '', "", ``
-new Boolean(); // true, false
-new Number(); // 1, 2, 3, ...
 */
 
-// Factory Function
-function createCircle(radius) {
-    return {
-        radius,
-        draw: function() {
-            console.log('draw');
-        }
-    };
-}
-const circle = createCircle(1);
-
-// Constructor Function
 function Circle(radius) {
     this.radius = radius;
     this.draw = function() {
         console.log('draw');
     }
 }
+
+Circle.call({}, 1);
+
+Circle.apply({}, [1, 2, 3]);
+
 const another = new Circle(1);
