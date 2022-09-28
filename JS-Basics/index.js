9041af6610601d5c221bebe31369b0bb6aee1531
dialecticsniff <Dialecticsniff@hotmail.com>
2022-09-28 16:20:40 -0700

    05 - Objects - 12 - String - 6.25

--- JS-Basics/index.js
+++ JS-Basics/index.js
@@ -1,91 +1,10 @@
 /*
-let address = {
-    street: 'a',
-    city: 'b',
-    zipCode: 'c'
-};
 
-
-
-
-
-let address = {
-    street: 'a',
-    city: 'b',
-    zipCode: 'c'
-};
-
-function createAddress(street, city, zipCode) {
-    return {
-        street,
-        city,
-        zipCode
-    };
-}
-
-
-
-let address = {
-    street: 'a',
-    city: 'b',
-    zipCode: 'c'
-};
-
-function CreateAddress(street, city, zipCode) {
-    this.street = street;
-    this.city = city;
-    this.zipCode = zipCode;
-}
-
-const createAddress = new CreateAddress('a', 'b', 'c')
-console.log(CreateAddress[street])
-
-
-///this also works
-console.log(createAddress('a', 'b', 'c'))
-
-function createAddress(street, city, zipCode) {
-    return {
-        street,
-        city,
-        zipCode
-    };
-}
-
-
-
-function CreateAddress(street, city, zipCode) {
-    this.street = street;
-    this.city = city;
-    this.zipCode = zipCode;
-}
-
-const address = new CreateAddress('a', 'b', 'c');
-
-console.log(address)
 */
+// String primitive
+const message = 'This is my\n first message';
 
-///let address = createAddress('a', 'b', 'c');
+// String object
+const another = new String('hi');
 
-let address = new Address('a', 'b', 'c');
-
-console.log(address)
-
-
-//factory function
-function createAddress(street, city, zipCode) {
-    return {
-        street,
-        city,
-        zipCode
-    };
-}
-
-
-//constructor function
-function CreateAddress(street, city, zipCode) {
-    this.street = street;
-    this.city = city;
-    this.zipCode = zipCode;
-}
-
+console.log(message)
