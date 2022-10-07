90e1c823c135b9c1e1913c586eff2a52559e5d18
dialecticsniff <Dialecticsniff@hotmail.com>
2022-10-06 20:07:01 -0700

    06 - Arrays - 01 - Introduction - .30

--- JS-Basics/index.js
+++ JS-Basics/index.js
@@ -1,18 +1,5 @@
 /*
- let gizmo = [
-    { label: '$', tooltip: 'Message', Color: Highlight, Box: size  },
-    { label: '$', tooltip: 'Message', Color: Highlight, Box: size  },
-    { label: '$', tooltip: 'Message', Color: Highlight, Box: size  }
-]
+
 
 */
 
-let priceRanges = [
-    { label: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerPerson: 10 }
-    { label: '$$', tooltip: 'Moderate', minPerPerson: 11, maxPerPerson: 20 }
-    { label: '$$$', tooltip: 'Expensive', minPerPerson: 21, maxPerPerson: 50 }
-]
-
-let restaurants = [
-    { averagePerPerson: 5 }
-]
