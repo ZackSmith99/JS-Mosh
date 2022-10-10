d08ef999a9ba2ada4375655bd936ca19295e014f
dialecticsniff <Dialecticsniff@hotmail.com>
2022-10-09 23:26:06 -0700

    06 - Arrays - 04 - Finding Elements (Reference Types) - 5.46

--- JS-Basics/index.js
+++ JS-Basics/index.js
@@ -1,12 +1,26 @@
 /*
+const courses = [
+    { id: 1, name: 'a'},
+    { id: 2, name: 'b'},
+];
 
+const course = courses.find(function(course) {
+    return course.name === 'a';
+});
+
+console.log(course);
 
 */
 
-const numbers = [1, 2, 3, 1, 4,]
 
-console.log(numbers.indexOf(1, 2));
-console.log(numbers.lastIndexOf(1));
 
-console.log(numbers.indexOf(1) !== -1);
-console.log(numbers.includes(1));
+const courses = [
+    { id: 1, name: 'a'},
+    { id: 2, name: 'b'},
+];
+
+const course = courses.findIndex(function(course) {
+    return course.name === 'a';
+});
+
+console.log(course);
