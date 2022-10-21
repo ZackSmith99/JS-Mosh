263e572a37cee6550d2665739e4b494b4cb03c7c
dialecticsniff <Dialecticsniff@hotmail.com>
2022-10-20 19:56:17 -0700

    06 - Arrays - 15 - Mapping an Array - 7.47

--- JS-Basics/index.js
+++ JS-Basics/index.js
@@ -1,9 +1,163 @@
 /*
-
-*/
-  
 const numbers = [1, -1, 2, 3];
 
 const filtered = numbers.filter(n => n >= 0); 
 
 console.log(filtered);
+
+
+
+
+
+
+const numbers = [1, -1, 2, 3];
+
+const filtered = numbers.filter(n => n >= 0); 
+
+filtered.map(n => '<li>' + n + '</li>')
+
+console.log(filtered);
+
+
+
+
+
+
+const numbers = [1, -1, 2, 3];
+
+const filtered = numbers.filter(n => n >= 0); 
+
+const items = filtered.map(n => '<li>' + n + '</li>')
+
+const html = items.join('');
+
+console.log(html);
+
+
+
+
+const numbers = [1, -1, 2, 3];
+
+const filtered = numbers.filter(n => n >= 0); 
+
+const items = filtered.map(n => '<li>' + n + '</li>')
+
+const html = '<ul>' + items.join('') + '</ul>'
+
+console.log(html);
+
+
+
+
+const numbers = [9, -1, 2, 3];
+
+const filtered = numbers.filter(n => n >= 0); 
+
+const items = filtered.map(n => {
+    const obj = { value: n };
+    return obj;
+})
+
+console.log(items);
+
+
+
+
+const numbers = [9, -1, 2, 3];
+
+const filtered = numbers.filter(n => n >= 0); 
+
+const items = filtered.map(n => {
+    const obj = { value: n };
+})
+
+console.log(items);
+
+
+
+
+
+
+
+
+const numbers = [9, -1, 2, 3];
+
+const filtered = numbers.filter(n => n >= 0); 
+
+const items = filtered.map(n => ({ value: n }));
+
+
+console.log(items);
+
+
+
+
+
+const numbers = [9, -1, 2, 3];
+
+numbers.filter(n => n >= 0).map 
+
+const items = filtered.map(n => ({ value: n }));
+
+
+console.log(items);
+
+
+
+
+const numbers = [9, -1, 2, 3];
+
+const filtered = numbers.filter(n => n >= 0); 
+
+const items = filtered.map(n => ({ value: n }));
+
+console.log(items);
+
+
+
+
+
+
+
+const numbers = [9, -1, 2, 3];
+
+numbers.filter(n => n >= 0).map 
+
+const items = filtered.map(n => ({ value: n }));
+
+console.log(items);
+
+
+
+const numbers = [9, -1, 2, 3];
+
+numbers.filter(n => n >= 0).map(n => ({ value: n }));
+
+
+console.log(items);
+
+
+
+
+
+const numbers = [9, -1, 2, 3];
+
+numbers
+    .filter(n => n >= 0)
+    .map(n => ({ value: n }));
+
+const items = filtered.map(n => ({ value: n }));
+
+console.log(items);
+
+*/
+  
+const numbers = [1, -1, 2, 3];
+
+const items = numbers
+    .filter(n => n >= 0)
+    .map(n => ({ value: n }))
+    .filter(obj => obj.value > 1)
+    .map(obj => obj.value );
+    
+console.log(items);
