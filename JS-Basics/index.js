426303d35f4561420f3fe104201bffbdb0762060
dialecticsniff <Dialecticsniff@hotmail.com>
2022-11-04 14:09:29 -0700

    06 - Array - 19 - Exercise 03 - Except - 2.08

--- JS-Basics/index.js
+++ JS-Basics/index.js
@@ -1,29 +1,17 @@
 /*
-const numbers = [1, 2, 2, 3, 4];
 
-function includes(array, searchElement) { 
-  let booleanz = false;
-  
-  for (let number of array ) { 
-    if (number === searchElement) {
-      booleanz = true;
-      }
-    }
-  return booleanz
-}
-
-console.log(includes(numbers, 2))
 */
   
+const numbers = [1, 2, 3, 4, 1, 1];
 
-const numbers = [1, 2, 2, 3, 4];
+const output = except(numbers, [1, 2, 3, 4]);
 
-console.log(includes(numbers, 1));
-
-function includes(array, searchElement) { 
+console.log(output);
+  
+function except(array, excluded) {
+  const output = [];
   for (let element of array)
-    if (element === searchElement)
-      return true;
-  return false;
+    if (!excluded.includes(element))
+      output.push(element);
+  return output;
 }
-
