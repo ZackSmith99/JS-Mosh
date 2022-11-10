e9a8eb055dd1345dbb4e2adbb96af58a2c5ac8ec
dialecticsniff <Dialecticsniff@hotmail.com>
2022-11-09 21:08:17 -0800

    06 - Arrays - 20 - Exercise 04 - Moving an Array - 6.31

--- JS-Basics/index.js
+++ JS-Basics/index.js
@@ -2,16 +2,30 @@
 
 */
   
-const numbers = [1, 2, 3, 4, 1, 1];
+const numbers = [1, 2, 3, 4];
 
-const output = except(numbers, [1, 2, 3, 4]);
+const output = move(numbers, 3, 0);
 
 console.log(output);
-  
-function except(array, excluded) {
-  const output = [];
-  for (let element of array)
-    if (!excluded.includes(element))
-      output.push(element);
-  return output;
+
+function move(array, index, offset) { 
+
+    let numbersTwo = [...numbers];
+    
+    function countArray(numbersTwo) { 
+        let count = 0;
+            for (let value of numbersTwo)
+            count++
+            return count;
+}
+
+    if ( (index + offset) < 0 || ( index + offset ) >= (countArray(numbersTwo)) )
+        return console.error('Invalid Offset');
+
+console.log(countArray(numbersTwo))
+
+let deletes = numbers.splice(index, 1);
+let convert = deletes.pop();
+let move = numbers.splice( (index + offset), 0, convert);
+return numbersTwo;
 }
